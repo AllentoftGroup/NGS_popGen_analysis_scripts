ca406543ec3dbd4881e7cebe844bacb7b8a7a2d2
Maria Novosolov <13299246+marianovosolov@users.noreply.github.com>
2020-03-27 11:46:56 +0100

    Updating documentation and some code

--- bash_scripts/summary_statistics_PS_mapping.sh
+++ bash_scripts/summary_statistics_PS_mapping.sh
@@ -1,27 +1,30 @@
 
 #the directory with all the bam files 
 IN=${1}
-#the directory where to save all the results
+#set the directory where to save all the results
 OUT=/science/willerslev/users-shared/science-snm-willerslev-rlw363/samtools_summary_merged
 
-#mkdir -p ${OUT}
+#create a new directory for the the files that will be created
+if [[! -f "${OUT}" ]]
+then
+	mkdir -p ${OUT}
+fi
+
 #run the samtools to get the number of reads mapped for each bam file and save it into a txt file
-#for i in picard_comb/*.bam
-#do
-#	echo "samtools flagstat ${i} > samtools_summary_merged/`basename ${i}|cut -f1-2 -d.`"_summary.txt""
-#done
+for i in picard_comb/*.bam
+do
+	echo "samtools flagstat ${i} > samtools_summary_merged/`basename ${i}|cut -f1-2 -d.`"_summary.txt""
+done
+
+
 #run the count of reads mapped per position and save into a csv file
 for i in ${IN}/*.bam
 do
 	echo $(/willerslev/software/samtools/samtools mpileup ${i}|cut -f4|sort -n|uniq -c > ${OUT}/`basename ${i}|cut -f1-2 -d.`"_coverage.csv")
 done
+
 #run an R script that extracts the values of reads and duplicates from the first type of file into a table that has the names of the sample, the original name of the sample (KXX or MXX) whether it's pod Kopista (K) or Pod Mercaru (M) and saves this table into an RDS
-#Rscript /science/willerslev/users-shared/science-snm-willerslev-rlw363/summary_stat_PS_mapping.R
+Rscript /science/willerslev/users-shared/science-snm-willerslev-rlw363/summary_stat_PS_mapping.R
+
 # In the same R script read each table from the second sametool run and calculate the weighted average for each sample to find out the coverage. Put the result into a table that has the sample name, the original sample name, the island it's from, and the weighted average. Save the table into an RDS.
 
-#> ${OUT}/`basename ${i}|cut -f1-2 -d.`"_coverage.csv"
-
-#for i in *.md.bam
-#do
-#    echo `samtools flagstat ${i}` >${i}.stat
-#done
